022d68f12d3b01415a74788a8fffc00158173f19
KevinKCD <kevinkcdoan@gmail.com>
2021-02-10 15:02:44 +0000

    Search Bar commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
   const API_KEY = "a33e014e75b0261963e1c6981f48ea3e";
+  const city_name = " "; //Empty need to initialise to User input from Search Bar
   const example = `api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`;
-  const city_name = " ";
 
+
+useEffect(async () => {
+  getLocation()
+}, []);
+const getLocation = async () => {
+  const reponse = await fetch(`api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`)
+  const data = reponse.json();
+
+  // Calls the API key and allows you call certain data. 
+  // Add parameters we need to call such as temp, day, sunny/rainy
+}
   return (
     <div className="App">
       <form className="Search-form">
-        <input classnName="Search-Bar" type= "text"/>
+        <input className="Search-Bar" type= "text"/>
         <button className= "Search-button" type = "submit">
           Search
           </button>

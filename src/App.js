1205302747d64c68113584988709dc26aef0ed03
KevinKCD <kevinkcdoan@gmail.com>
2021-02-11 13:11:02 +0000

    Errors

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const API_KEY = "a33e014e75b0261963e1c6981f48ea3e";
-  const city_name = " "; //Empty need to initialise to User input from Search Bar
-
+  //var city_name = document.getElementById("city_name").value; //Empty need to initialise to User input from Search Bar
+  const [locations, setLocations] = useState([]);
 
 useEffect(async () => {
   getLocation()
 }, []);
+
 const getLocation = async () => {
-  const reponse = await fetch(`api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`) //Calls current Weather API
-  const data = reponse.json();
+  const reponse = await fetch(`api.openweathermap.org/data/2.5/weather?q={London}&appid=${API_KEY}`) //Calls current Weather API
+  const data = await reponse.json();
+  console.log(data);
   // Calls the API key and allows you call certain data. 
 }
+
   return (
     <div className="App">
       <form className="Search-form">
-        <input className="city_name" type= "text"/>
+        <input className="Search-bar" type= "text" id="city_name"/>
         <button className= "Search-button" type = "submit">
           Search
           </button>
       </form>
+      {locations.map(reciple =>(
+
+      <Locations />
+      ))};
     </div>
   );
+
 }
 
 export default App;

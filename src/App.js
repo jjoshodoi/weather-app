23d77de77272a26c73f3ad2279c6d91236cdb8e9
joshuaodoi <joshuaodoi@hotmail.co.uk>
2021-02-11 16:20:39 +0000

    upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import SearchText from "./search";
 
 function App() {
   const API_KEY = "a33e014e75b0261963e1c6981f48ea3e";
-  const city_name = "London"; //Empty need to initialise to User input from Search Bar
+  var city_name = " "; //Empty need to initialise to User input from Search Bar
+  console.log(city_name);
   const example = `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`;
   // const example = `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`;
 
-  useEffect(async () => {
+  const [searchText, setSearchText] = useState(() => " ");
+  
+  useEffect(() => {
     getLocation();
-  }, []);
-  const getLocation = async () => {
-    const reponse = await fetch(example);
+  }, [city_name]);
+
+  const getLocation = async (searchText) => {
+    console.log(searchText);
+    const reponse = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${API_KEY}`
+    );
     const data = reponse.json();
     console.log(data);
 
@@ -20,14 +28,35 @@ function App() {
     // Add parameters we need to call such as temp, day, sunny/rainy
   };
 
+  const handleChanged = (e) => {
+    // Handle Changed Code
+    setSearchText(e.target.value);
+    // console.log(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    // Submit code
+    e.preventDefault();
+    city_name = searchText;
+    // console.log(city_name);
+    getLocation();
+  };
+
   return (
     <div className="App">
-      <form className="Search-form">
-        <input className="Search-Bar" type="text" />
-        <button className="Search-button" type="submit">
+      <form className="Search-form" onSubmit={handleSubmit}>
+        <input
+          className="Search-Bar"
+          type="text"
+          value={searchText}
+          onChange={handleChanged}
+          placeholder="Enter City ... e.g. London, Birmingham, Moscow"
+        />
+        <button onClick={handleSubmit} className="Search-button" type="submit">
           Search
         </button>
       </form>
+      <SearchText getLocation={getLocation}/>
     </div>
   );
 }

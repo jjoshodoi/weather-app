3f3052c1e780b3911fc3d801b4d30c531f8a8de4
joshuaodoi <joshuaodoi@hotmail.co.uk>
2021-02-11 13:57:08 +0000

    London Works

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
   const API_KEY = "a33e014e75b0261963e1c6981f48ea3e";
-  var city_name = document.getElementById("city_name").value; //Empty need to initialise to User input from Search Bar
-  const [locations, setLocations] = useState([]);
+  const city_name = "London"; //Empty need to initialise to User input from Search Bar
+  const example = `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`;
+  // const example = `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`;
 
-useEffect(async () => {
-  getLocation()
-}, []);
+  useEffect(async () => {
+    getLocation();
+  }, []);
+  const getLocation = async () => {
+    const reponse = await fetch(example);
+    const data = reponse.json();
+    console.log(data);
 
-const getLocation = async () => {
-  const reponse = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`
-    ); //Calls current Weather API
-  const data = await reponse.json();
-  console.log(data);
-  // Calls the API key and allows you call certain data. 
-}
+    // Calls the API key and allows you call certain data.
+    // Add parameters we need to call such as temp, day, sunny/rainy
+  };
 
   return (
     <div className="App">
       <form className="Search-form">
-        <input className="Search-bar" type= "text" id="city_name"/>
-        <button className= "Search-button" type = "submit">
+        <input className="Search-Bar" type="text" />
+        <button className="Search-button" type="submit">
           Search
         </button>
       </form>
     </div>
   );
 }
+
 export default App;

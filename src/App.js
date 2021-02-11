3023c4dbbd81dba9a3553461ba0b50db66834e78
KevinKCD <kevinkcdoan@gmail.com>
2021-02-11 12:23:23 +0000

    Re-commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,15 @@ import { useEffect } from 'react';
 function App() {
   const API_KEY = "a33e014e75b0261963e1c6981f48ea3e";
   const city_name = " "; //Empty need to initialise to User input from Search Bar
-  const example = `api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`;
 
 
 useEffect(async () => {
   getLocation()
 }, []);
 const getLocation = async () => {
-  const reponse = await fetch(`api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`)
+  const reponse = await fetch(`api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`) //Calls current Weather API
   const data = reponse.json();
-
   // Calls the API key and allows you call certain data. 
-  // Add parameters we need to call such as temp, day, sunny/rainy
 }
   return (
     <div className="App">

49ee7813836b7262877123f1bbf5838ee022ac9b
joshuaodoi <joshuaodoi@hotmail.co.uk>
2021-02-14 06:26:08 +0000

    Displays the main attribute on card

--- src/components/tempTiles/tempByHour.jsx
+++ src/components/tempTiles/tempByHour.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TempCard from "./tempcard";
 
 const TempByHour = ({
   oneCallDataFromApi,
@@ -10,30 +11,23 @@ const TempByHour = ({
   const timeDifference =
     oneCallDataFromApi && oneCallDataFromApi.timezone_offset / 3600;
 
-  var weatherAttributeForCard = findMainWeatherAttribute(oneCallDataFromApi);
+  // var weatherAttributeForCard = findMainWeatherAttribute(oneCallDataFromApi);
   return (
     <div className="temp-tile">
       {oneCallDataFromApi &&
         oneCallDataFromApi.hourly.slice(0, 24).map((
           hour,
           index //How many values are we to display
-          // TODO: Get individual data for each tile
-          // Have a sub component that we pass data into!
         ) => (
-          <div className="tile-border">
-            <div className="card">
-              {index === 0
-                ? "Now"
-                : currentHour + index + timeDifference > 23
-                ? currentHour + index + timeDifference - 24
-                : currentHour + index + timeDifference}
-            </div>
-            <div>{weatherAttributeForCard}</div>
-            <div>{`${kelvinToCelcius(
-              //Change key to be unique
-              hour.temp
-            )}°C`}</div>
-          </div>
+          <TempCard
+            key={index} // Uniquely identify each Card we make
+            indexNum={index}
+            hour={hour}
+            currentHour={currentHour}
+            timeDifference={timeDifference}
+            oneCallDataFromApi={oneCallDataFromApi}
+            kelvinToCelcius={kelvinToCelcius}
+          />
         ))}
     </div>
   );

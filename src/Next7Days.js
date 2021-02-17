d7f8e914ab551e7032e2ba8610b58c32323ab93f
joshuaodoi <joshuaodoi@hotmail.co.uk>
2021-02-17 17:54:25 +0000

    sorted days on 7 weeks

--- src/Next7Days.js
+++ src/Next7Days.js
@@ -18,38 +18,42 @@ const Next7DaysView = ({
   return (
     <div>
       <h1>Next 7 Days</h1>
-      <div className = "center">
-          <div className= "temp-tile">
-            {oneCallDataFromApi &&
-              oneCallDataFromApi.daily.slice(0, 7).map((day, index) => (
-                  <div className ="tile-border" key={index}>
-                    <div className ="center">
-                    {today + index === 0
-                      ? "Today"
-                      : index === 1
-                      ? "Tomorrow"
-                      : days[today + index]}{" "}
-                      </div>
-                          <div className="card">
-                            {(() => {
-                            switch (day.weather[0].main) {
-                              case "Clouds":
-                                return  <AiFillCloud size = "1x"/>;   
-                              case "Rain":
-                                return  <IoRainy size="1x"/>;
-                              case "Snow":
-                                return <BiCloudSnow size="1x"/>  
-                              case "Clear":
-                                return <TiWeatherSunny size="1x"/>  
-                            }
-                          })()}     
-                          </div>
-                    <h4>{`Highs: ${kelvinToCelcius(day.temp.max)}°C Lows: ${kelvinToCelcius(day.temp.min)}°C`}</h4>
-                </div>  
-              ))}
-          </div>
+      <div className="center">
+        <div className="temp-tile">
+          {oneCallDataFromApi &&
+            oneCallDataFromApi.daily.slice(0, 7).map((day, index) => (
+              <div className="tile-border" key={index}>
+                <div className="center">
+                  {today + index === today
+                    ? "Today"
+                    : index === 1
+                    ? "Tomorrow"
+                    : days[
+                        today + index > 6 ? today + index - 7 : today + index
+                      ]}{" "}
+                </div>
+                <div className="card">
+                  {(() => {
+                    switch (day.weather[0].main) {
+                      case "Clouds":
+                        return <AiFillCloud size="1x" />;
+                      case "Rain":
+                        return <IoRainy size="1x" />;
+                      case "Snow":
+                        return <BiCloudSnow size="1x" />;
+                      case "Clear":
+                        return <TiWeatherSunny size="1x" />;
+                    }
+                  })()}
+                </div>
+                <h4>{`Highs: ${kelvinToCelcius(
+                  day.temp.max
+                )}°C Lows: ${kelvinToCelcius(day.temp.min)}°C`}</h4>
+              </div>
+            ))}
         </div>
-        <SelectDay setCurrentView={setCurrentView} currentView={currentView}/>
+      </div>
+      <SelectDay setCurrentView={setCurrentView} currentView={currentView} />
     </div>
   );
 };
